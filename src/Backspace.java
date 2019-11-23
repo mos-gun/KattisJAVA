f43901c28af2641b6db535333f64cd75cab34d7d
360s <mosgun.mahdere@gmx.net>
2019-11-23 18:46:54 +0100

    Solved: Backspace

--- src/Backspace.java
+++ src/Backspace.java
@@ -1,117 +1,57 @@
-//import java.util.Scanner;
-//
-//public class Backspace {
-//	public static void main(String[] args) {
-//		Scanner scanner = new Scanner(System.in);                        //Wrong answer at test case 4
-//
-//		StringBuilder sb = new StringBuilder();
-//		String line = scanner.nextLine();
-//		int len = line.length();
-//		int counter = 0;
-//		for (int i = len - 1; i >= 0; i--) {
-//			if (line.charAt(i) == '<') {
-//				counter++;
-//			} else {
-//				if (i == 0 && line.charAt(1) == '<') {
-//					break;
-//				} else {
-//					i -= counter;
-//					if (line.charAt(i) == '<') {
-//						i++;
-//						counter = 0;
-//						continue;
-//					} else {
-//						sb.append(line.charAt(i));
-//						counter = 0;
-//					}
-//				}
-//			}
-//		}
-//		sb.reverse();
-//		System.out.println(sb);
-//	}
-//}
-//
-//
-////
-//////		StringBuilder sb = new StringBuilder();
-//////		sb.append(scanner.nextLine());
-//////		String input = sb.toString();
-//////		int posOfShift = sb.indexOf("<");
-//////		while (posOfShift != - 1 && input.contains("<")) {                        // Time Limit Exceeded
-//////			sb.deleteCharAt(posOfShift);
-//////			sb.deleteCharAt(posOfShift - 1);
-//////			posOfShift = sb.indexOf("<");
-//////		}
-//////		System.out.println(sb);
-////
-////
-//////	String line;													// Time Limit Exceeded
-//////		line = scanner.nextLine();
-//////				while (line.contains("<")) {
-//////				line = line.substring(0, line.indexOf('<') - 1) + line.substring(line.indexOf('<') + 1);
-//////				}
-//////				System.out.println(line);
-//////				}
-////
-////
-//////		String line;												// Time Limit Exceeded
-//////		line = scanner.nextLine();
-//////		while (line.contains("<")) {
-//////			System.out.println(line);
-//////			line = line.replace(String.valueOf(line.charAt(line.indexOf('<') - 1)), "");
-//////			System.out.println(line);
-//////			line = line.replaceFirst(String.valueOf(line.charAt(line.indexOf('<'))), "");
-//////			System.out.println(line);
-//////		}
-//////		System.out.println(line);
-////
-////
-//////		StringBuilder sb = new StringBuilder();
-//////		sb.append(scanner.nextLine());
-//////		while (sb.toString().contains("<")) {						// Time Limit Exceeded
-//////			sb.deleteCharAt(sb.indexOf("<")-1);
-//////			sb.deleteCharAt(sb.indexOf("<"));
-//////		}
-//////		System.out.println(sb);	
-////
-////
-//////		StringBuilder sb = new StringBuilder();
-//////		sb.append(scanner.nextLine());
-//////		for (int i = 0; i < sb.length(); i++) {						// Time Limit Exceeded
-//////			if(sb.charAt(i) == '<'){
-//////				sb.deleteCharAt(i);
-//////				sb.deleteCharAt(i-1);
-//////				i-=2;
-//////			}
-//////		}
-//////		System.out.println(sb);
-////
-////
-//////	Scanner scanner = new Scanner(System.in);						//Wrong answer at test case 4
-//////
-//////	StringBuilder sb = new StringBuilder();
-//////	String line = scanner.nextLine();
-//////	int len = line.length();
-//////	int counter = 0;
-//////		for (int i = len - 1; i >= 0; i--) {
-//////				if (line.charAt(i) == '<') {
-//////				counter++;
-//////				} else {
-//////				if (i == 0 && line.charAt(1) == '<') {
-//////				break;
-//////				} else {
-//////				i -= counter;
-//////				if (line.charAt(i) == '<') {
-//////				i++;
-//////				counter = 0;
-//////				continue;
-//////				} else {
-//////				sb.append(line.charAt(i));
-//////				counter = 0;
-//////				}
-//////				}
-//////				}
-//////				}
-//////				sb.reverse();
-//////				System.out.println(sb);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Backspace {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out = new PrintWriter(System.out);
+		String input;
+		StringBuilder sbInput, sbOutput;
+		char[] arrInput;
+		int lenOfSbInput, counter;
+
+		input = in.readLine();
+		sbInput = new StringBuilder(input);
+		sbInput.reverse();
+		arrInput = sbInput.toString().toCharArray();
+		lenOfSbInput = arrInput.length;
+		sbOutput = new StringBuilder();
+		counter = 0;
+
+		for (int i = 0; i < lenOfSbInput; i++) {
+			if (arrInput[i] == '<') {
+				counter += 1;
+			} else {
+				if (counter != 0) {
+					i++;
+					counter--;
+					while (counter > 0 && i < lenOfSbInput) {
+						if(arrInput[i] == '<') {
+							i++;
+							counter++;
+						} else {
+							i++;
+							counter--;
+						}
+					}
+					if (i >= lenOfSbInput) break;
+					if (arrInput[i] == '<') {
+						counter = 1;
+					} else {
+						sbOutput.append(arrInput[i]);
+						counter = 0;
+					}
+				} else {
+					sbOutput.append(arrInput[i]);
+				}
+			}
+		}
+		sbOutput.reverse();
+		out.println(sbOutput);
+
+		out.flush();
+		out.close();
+	}
+}
