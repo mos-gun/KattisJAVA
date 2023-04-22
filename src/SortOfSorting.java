1b484c43315ddc96ea2bf30c84451da1d156944d
mos-gun <mosgun.mahdere@gmx.net>
2023-04-22 16:07:02 +0200

    Solved problem <SortOfSorting>

--- src/SortOfSorting.java
+++ src/SortOfSorting.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
@@ -8,8 +7,6 @@ public class SortOfSorting {
         Scanner scanner = new Scanner(System.in);
         int numOfInputs;
         ArrayList<String> listInput;
-
-        numOfInputs = -1;
 
         while ((numOfInputs = scanner.nextInt()) != 0) {
             scanner.nextLine();
@@ -25,6 +22,5 @@ public class SortOfSorting {
             }
             System.out.println();
         }
-
     }
 }
