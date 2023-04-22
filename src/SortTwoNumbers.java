b224d310c8348c57b8f27cc08d848f2dfba4da9d
mos-gun <mosgun.mahdere@gmx.net>
2023-04-22 14:31:50 +0200

    Added solved problem <SortTwoNumbers>

---
+++ src/SortTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SortTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        if(num1 < num2) System.out.println(num1 + " " + num2);
+        else System.out.println(num2 + " " + num1);
+    }
+}
