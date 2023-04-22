ac03cd5a9c716f1b2625af55e0d30319cf859a79
mos-gun <mosgun.mahdere@gmx.net>
2023-04-22 14:29:40 +0200

    Added solved problem <KnightPacking>

---
+++ src/KnightPacking.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class KnightPacking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.nextInt() % 2 == 0) System.out.println("second");
+        else System.out.println("first");
+    }
+}
